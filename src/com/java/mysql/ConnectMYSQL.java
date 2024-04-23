38e822f02556fede7d200be55bda1172c2b9fcb7
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-23 18:23:00 +0900

    データベースとSQL -Lesson2- SELECT文の実行 追加:gitにcommitしました

--- src/com/java/mysql/ConnectMYSQL.java
+++ src/com/java/mysql/ConnectMYSQL.java
@@ -2,7 +2,9 @@ package com.java.mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectMYSQL {
 
@@ -11,8 +13,18 @@ public class ConnectMYSQL {
 		String user = "root";
 		String password = "passw0rd!";
 		try (Connection conn = DriverManager.getConnection(url,user,password);){
-
-			System.out.println("MYSQL connection is success!!");
+			
+			Statement st = conn.createStatement(); //データベースを送信する役割
+			ResultSet res = st.executeQuery("SELECT * FROM PRODUCT"); //データベースから結果を取得する役割
+			
+			while(res.next()) {
+				int id = res.getInt("ID");
+				String name = res.getString("NAME");
+				int price = res.getInt("PRICE");
+				int categoryId = res.getInt("CATEGORY_ID");
+				System.out.println(String.format("ID:%d NAME:%s PRICE:%d CATEGORY_ID:%d", id,name,price,categoryId));
+			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
