3c048e16e492d802458c780f863506dd13ef06ac
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-24 09:40:17 +0900

    データベースとSQL -Lesson2- DDL・DML文の実行 DROP文の実行　追加:gitにcommitしました

--- src/com/java/mysql/ConnectMYSQL.java
+++ src/com/java/mysql/ConnectMYSQL.java
@@ -2,7 +2,7 @@ package com.java.mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,16 +12,12 @@ public class ConnectMYSQL {
 		String url = "jdbc:mysql://localhost:3306/SHOPPING?connectionTimezone=JST";
 		String user = "root";
 		String password = "passw0rd!";
-		try (Connection conn = DriverManager.getConnection(url,user,password);){
-			
-			Statement st = conn.createStatement(); //データベースを送信する役割
-			ResultSet res = st.executeQuery("SELECT AVG(PRICE) AS 'AVG' FROM PRODUCT WHERE CATEGORY_ID =6;"); //データベースから結果を取得する役割
-			
-			while(res.next()) {
-				int avg = res.getInt("AVG");
-				System.out.println(String.format("AVG:%d", avg));
-			}
-			
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+
+			Statement st = conn.createStatement(); // データベースを送信する役割
+			int count = st.executeUpdate("DROP TABLE PRODUCT");
+			System.out.println("結果:" + count);
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
