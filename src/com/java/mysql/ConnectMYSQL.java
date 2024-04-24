7d70ce72f8e3d493792dc96998c93d0ca53ed2c5
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-24 10:09:41 +0900

    データベースとSQL -Lesson2- SELECT文の実行 追加:gitにcommitしました

--- src/com/java/mysql/ConnectMYSQL.java
+++ src/com/java/mysql/ConnectMYSQL.java
@@ -14,14 +14,36 @@ public class ConnectMYSQL {
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			
 			String sql =
-					"CREATE TABLE `PRODUCT` (" +
-				    "`ID` INT NOT NULL AUTO_INCREMENT," +
-					"`NAME` VARCHAR(40) NOT NULL," +
-				    "`PRICE` INT NOT NULL DEFAULT 0," +
-					"`CATEGORY_ID` INT ," +
-				    "PRIMARY KEY (`ID`)" +
+					"INSERT INTO PRODUCT (" +
+					" NAME,PRICE,CATEGORY_ID" +
+					")" +
+					" VALUES(" +
+					"  '味のもとのもと',500,3" +
+					"),(" +
+					"'そっくりクレヨン',300,4" +
+					"),(" +
+					"'超サイクロン掃除機',30000,6" +
+					"),(" +
+					"'Java入門編',1500,1" +
+					"),(" +
+					"'アンキパン',200,NULL" +
+					"),(" +
+					"'生キャラメルプリン',500,3" +
+					"),(" +
+					"'手を動かして学ぶJava',2500,1" +
+					"),(" +
+					"'できるJavaプログラミング入門',2000,1" +
+					"),(" +
+					"'日曜産業セット',5000,5" +
+					"),(" +
+					"'ここほれワイヤー',2500,6" +
+					"),(" +
+					"'タイムカメラ',12000,6" +
+					"),(" +
+					"'JavaScriptを学ぼう',1800,1" +
+					"),(" +
+					"'サンプル',0,NULL" +
 					")";
-
 			Statement st = conn.createStatement(); // データベースを送信する役割
 			int count = st.executeUpdate(sql);
 			System.out.println("結果:" + count);
